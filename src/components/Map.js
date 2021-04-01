4041344cb414b38f23e178a8b84b79ebff4bd7e9
oscar.diaz <os.diazl@alumnos.duoc.cl>
2021-04-01 01:35:23 -0300

    fixed

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,7 +10,7 @@ const Map = ({eventData, center, zoom}) => {
 
 
     const markers = eventData.map( ev => {
-        if(ev.categories[0].id === 15 ||  ev.categories[0].id === 8  || ev.categories[0].id === 12 && ev.id !== "EONET_354" ){
+        if((ev.categories[0].id === 15 ||  ev.categories[0].id === 8  || ev.categories[0].id === 12) &&  ev.id !== "EONET_354" ){
             return <LocationMarker 
                         key={ev.id}
                         lat={ev.geometries[0].coordinates[1]} 
