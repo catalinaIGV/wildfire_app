6a51bc6f8006b0dfda75a4e2cf7b66937156e10f
oscar.diaz <os.diazl@alumnos.duoc.cl>
2021-04-01 01:40:15 -0300

    finidhed

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,12 @@ import {Icon} from "@iconify/react";
 import locationIcon from "@iconify/icons-mdi/fire-alert"; 
 import icebergIcon from '@iconify-icons/openmoji/iceberg';
 import volcanoIcon from '@iconify-icons/emojione/volcano';
+import fireIcon from '@iconify-icons/emojione/fire';
 
 const Header = () => {
     return (
         <header className="header">
-            <h1><Icon icon={locationIcon}/> <Icon icon={icebergIcon}/>  <Icon icon={volcanoIcon}/> Wildfire, IceBerg & Volcanos Tracker (Powered by NASA)</h1>
+            <h1><Icon icon={fireIcon}/> <Icon icon={icebergIcon}/>  <Icon icon={volcanoIcon}/> Wildfire, IceBerg & Volcanos Tracker (Powered by NASA)</h1>
         </header>
     )
 }
--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -2,6 +2,7 @@ import {Icon} from "@iconify/react";
 import locationIcon from "@iconify/icons-mdi/fire-alert"; 
 import icebergIcon from '@iconify-icons/openmoji/iceberg';
 import volcanoIcon from '@iconify-icons/emojione/volcano';
+import fireIcon from '@iconify-icons/emojione/fire';
 
 
 
@@ -11,7 +12,7 @@ const LocationMarker = ({lat, long, onClick, categoryId}) => {
     return (
         <div className="location-marker" onClick={onClick}>
 
-            { (categoryId === 8) ? <Icon icon={locationIcon} className="location-icon"></Icon> : (categoryId === 15) ? <Icon icon={icebergIcon} className="location-icon"></Icon> : <Icon icon={volcanoIcon} className="location-icon" /> }
+            { (categoryId === 8) ? <Icon icon={fireIcon} className="location-icon"></Icon> : (categoryId === 15) ? <Icon icon={icebergIcon} className="location-icon"></Icon> : <Icon icon={volcanoIcon} className="location-icon" /> }
             
         </div>
     )
